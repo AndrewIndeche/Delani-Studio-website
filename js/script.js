2d858e179148c2d5b4f0899a4c1d6c03ce6626c5
AndrewIndeche <indecheandrew@gmail.com>
2021-07-26 10:56:07 +0300

    Add Cards

---
+++ js/script.js
@@ -0,0 +1,8 @@
+<form action="mailto:you@yourdmainhere.com" method="post" enctype="text/plain" >
+FirstName:<input type="text" name="AllNames">
+Email:<input type="text" name="Email">
+Your message here::<input type="text" name="message">
+<input type="submit" name="submit" value="Submit">
+return alert( AllNames + " we have received your message. Thank you fro reaching out to us".+'**')
+}
+</form>
