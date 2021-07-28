4bdf7c127054f42408ee5b85e15d9fa0f669f25c
AndrewIndeche <indecheandrew@gmail.com>
2021-07-28 21:37:31 +0300

    Add for function arguments

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+var name = $(".name").val();
+ var email = $(".email").val();
+ var message = $(".message").val();
+
+ $(document).ready(function(){
+ ('.submit').click(function(){
+   event.preventDefault
+if (("email.length") >5 && email.includes ('@') && email.includes ('.')){
+  return alert('Email is valid')
+} else { alert ('Enter a valid email')
+}
+if ($("input#NAME").val() && $("input#EMAIL").val() && $("input#EMAIL").val){
+
+  //alert ("Dear" + name + "thank you for your message.We will get back to you");
+ //}
+  //else {
+  //alert("Please fill in all the fields!");
+ //}
+
+
+//  $("#designi").click(function(){
+//  $("#design text").show(function () {;
+ //$("#designi").hide(fast);
+//});
+//  $("#developmenti").click(function(){
+//  $("#development").show(function () {;
+  //$("#developmenti").hide(fast);
+//});
+  //$("#producti").click(function(){
+//  $("#product_mgt").show(function () {;
+//  $("#producti").hide(fast);
+//});
+
+  //  function processForm(e) {
+  //  e.preventDefault();
+
+  //  var name = $("input#name").val();
+  //  var email = $("input#email").val();
+  //  var message = $("input#message").val();
+  //  if (("input#name").val() || ("input#email").val() || ("input#message").val() ){
+    //alert ("Dear" + name + "thank you for your message.We will get back to you");
+  //  }
+  //  else {
+  //  alert("Please fill in all the fields!");
+  //  }
+  //});
