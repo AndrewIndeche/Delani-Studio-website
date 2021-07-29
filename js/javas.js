e0f51d998d39ea82d9d31ab50e0ce16b00999a7b
AndrewIndeche <indecheandrew@gmail.com>
2021-07-29 21:43:05 +0300

    Add hover effect

---
+++ js/javas.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $("#design").click(function(){
+    $(".showp").toggle();
+     $("#design image").toggle();
+ })
+ $(document).ready(function() {
+   $("#development").click(function(){
+     $("#showpr").toggle();
+      $("#product_icon").toggle();
+  })
+  $(document).ready(function() {
+    $("#productm").click(function(){
+      $("#showpr").toggle();
+       $("#product_icon").toggle();
+   })
