69b79ff9ffbb54adba280d729a3db11c5495d35b
AndrewIndeche <indecheandrew@gmail.com>
2021-07-30 18:43:28 +0300

    Add slide effect

---
+++ js/script.js
@@ -0,0 +1,104 @@
+  $(document).ready(function(){
+    $("#designi").click(function(){
+      $("#designi").slideDown('1500').hide('1500');
+      $("#showp").show('1500');
+    });
+    $("#showp").click(function(){
+      $("#showp").slideUp('1500');
+      $("#designi").show('1500');
+    });
+  });
+  $(document).ready(function(){
+    $("#developmenti").click(function(){
+      $("#developmenti").slideDown('1500').hide('1500');
+      $("#showd").show('1500');
+    });
+    $("#showd").click(function(){
+      $("#showd").slideUp('1500');
+      $("#developmenti").show('1500');
+    });
+  });
+  $(document).ready(function(){
+    $("#producti").click(function(){
+      $("#producti").slideDown('1500').hide('1500');
+      $("#showpr").show('1500');
+    });
+    $("#showpr").click(function(){
+      $("#showpr").slideUp('1500');
+      $("#producti").show('1500');
+    });
+  });
+
+
+
+
+
+
+
+
+
+  //   $(document).ready(function() {
+    //  $('#work1').mouseover(function() {
+      //  $('overlay').show();
+    //  }).mouseout(function() {
+        //$('.overlay1').hide();
+  //});
+    //  $(document).ready(function() {
+      // $('#work2').mouseover(function() {
+        // $('.overlay2').show();
+  //}).mouseout(function() {
+      //  $('.overlay2').hide();
+  //});
+    //  $(document).ready(function() {
+       //$('#work3').mouseover(function() {
+      //  $('.overlay3').show();
+  //}).mouseout(function() {
+      //  $('.overlay3').hide();
+  //});
+      //$(document).ready(function() {
+      // $('#work4').mouseover(function() {
+      //  $('.overlay4').show();
+  //}).mouseout(function() {
+        //$('.overlay4').hide();
+  //});
+      //$(document).ready(function() {
+      // $('#work5').mouseover(function() {
+      //  $('.overlay5').show();
+  //}).mouseout(function() {
+      //  $('.overlay5').hide();
+  //});
+    //  $(document).ready(function() {
+      // $('#work6').mouseover(function() {
+      //  $('.overlay6').show();
+  //}).mouseout(function() {
+        //$('.overlay6').hide();
+  //});
+    //  $(document).ready(function() {
+      // $('#work7').mouseover(function() {
+      //  $('.overlay7').show();
+  //}).mouseout(function() {
+        //$('.overlay7').hide();
+//  });
+
+    //  $('#work8').mouseover(function() {
+      //  $('.overlay8').show();
+  //}).mouseout(function() {
+        //$('.overlay8').hide();
+  //});
+    //  $(document).ready(function(){
+    //    $("form#form34A").submit(function(event){
+      //  event.preventDefault();
+      //   var name = $("input#name").val();
+      //   var email = $("input#email").val();
+      //   var message = $("input#message").val();
+    //  }
+        // if ($("#name").val() && $("#email").val() && $("#email").val()) {
+        //  alert ("Dear"+ name + " thank you for your message.we will be sure to get back to you");
+      //  }
+      //  else {
+        //  alert ("please fill in all fields");
+      //  }
+
+    //  });
+
+ //});
